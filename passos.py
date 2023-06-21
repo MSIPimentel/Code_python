3202f05529bee59a7295ebd08e23e53e010898d1
MSIPimentel <marlonufpasi@gmail.com>
2023-06-21 14:33:46 -0300

    modificar arquivos

---
+++ passos.py
@@ -0,0 +1,7 @@
+print("Praticar Git")
+n1 = int(input("Informe o primeiro número:"))
+n2 = int(input("Informe o segundo nũmero:"))
+n3 = int(input("Informe o terceiro número:"))
+s = n1 + n2 + n3
+
+print(f'Números {n1}, {n2},{n3}')
